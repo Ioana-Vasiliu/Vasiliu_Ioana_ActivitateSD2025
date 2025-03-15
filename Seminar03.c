a7a1dc155f0ae03a137bf52ba1726d3dfaed24d3
Delia <delia@example.com>
2025-03-15 19:23:07 +0200

    Seminar 03

---
+++ Seminar03.c
@@ -0,0 +1,134 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+typedef struct StructuraMasina Masina;
+
+void afisareMasina(Masina masina) {
+
+	printf("\nId: %d \n", masina.id);
+	printf("Numar Usi: %d \n", masina.nrUsi);
+	printf("Pret: %.2f \n", masina.pret);
+	printf("Model: %s \n", masina.model);
+	printf("Nume Sofer: %s \n", masina.numeSofer);
+	printf("Serie: %c \n", masina.serie);
+
+}
+
+void afisareVectorMasini(Masina* masini, int nrMasini) {
+	for (int i = 0;i < nrMasini;i++) {
+		afisareMasina(masini[i]);
+	}
+
+}
+
+//shallow copy
+
+//functia de adaugare in vector
+void adaugaMasinaInVector(Masina** masini, int* nrMasini, Masina masinaNoua) {
+	Masina* vector = (Masina*)malloc(sizeof(Masina) * ((*nrMasini) + 1));
+	if (!vector) {
+		printf("Eroare alocare memorie!\n");
+		return;
+	}
+
+	for (int i = 0; i < (*nrMasini); i++) {
+		vector[i] = (*masini)[i]; // Shallow copy
+	}
+
+	vector[*nrMasini] = masinaNoua;
+
+	// Deep copy pentru stringuri
+	vector[*nrMasini].model = (char*)malloc(strlen(masinaNoua.model) + 1);
+	strcpy(vector[*nrMasini].model, masinaNoua.model);
+
+	vector[*nrMasini].numeSofer = (char*)malloc(strlen(masinaNoua.numeSofer) + 1);
+	strcpy(vector[*nrMasini].numeSofer, masinaNoua.numeSofer);
+
+	(*nrMasini)++;
+	free(*masini);
+	(*masini) = vector;
+}
+
+Masina citireMasinaFisier(FILE* file) {
+	Masina masina;
+	char separator[] = ",\n";
+	char buffer[100];
+	fgets(buffer, 100, file);
+
+
+	char* token = strtok(buffer, separator);
+	masina.id = atoi(token);
+
+	char* token1 = strtok(NULL, separator);
+	masina.nrUsi = atoi(token1);
+
+	char* token2 = strtok(NULL, separator);
+	masina.pret = atof(token2);
+
+	char* token3 = strtok(NULL, separator);
+	masina.model = malloc(sizeof(char) * (strlen(token3) + 1));
+	strcpy_s(masina.model, strlen(token3) + 1, token3);
+
+	char* token4 = strtok(NULL, separator);
+	masina.numeSofer = malloc(sizeof(char) * (strlen(token4) + 1));
+	strcpy_s(masina.numeSofer, strlen(token4) + 1, token4);
+
+	char* token5 = strtok(NULL, separator);
+	masina.serie = token5[0];
+
+	return masina;
+}
+
+Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
+	FILE* f = fopen(numeFisier, "r");
+	Masina* masini = NULL;
+	while (!feof(f)) {
+		Masina m = citireMasinaFisier(f);
+		adaugaMasinaInVector(&masini, nrMasiniCitite, m);
+		free(m.model);
+		free(m.numeSofer);
+	}
+	fclose(f);
+	return masini;
+
+}
+
+void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
+	for (int i = 0;i < *nrMasini;i++) {
+		free((*vector)[i].model);
+		free((*vector)[i].numeSofer);
+
+
+	}
+	free(*vector);
+	*vector = NULL;
+	*nrMasini = 0;
+
+
+}
+
+int main() {
+	Masina* masini = NULL;
+	int nrMasini = 0;
+
+	//creare vector
+	masini = citireVectorMasiniFisier("masini.txt", &nrMasini);
+	afisareVectorMasini(masini, nrMasini);
+	dezalocareVectorMasini(&masini, &nrMasini);
+
+
+
+	return 0;
+}
